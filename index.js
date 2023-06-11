bdc93743bbdeeede3e2a7c650cb4b6d9189f8605
abigail <abigail.wamai@student.moringaschool.com>
2023-06-11 20:49:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+const yourName = "Wamai"
+newHeader.textContent = ('$yourName is the champion!')
+document.body.appendChild(newHeader)
